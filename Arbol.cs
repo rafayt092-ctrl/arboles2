82d89c7a3905670952c6719cf95f1dad0460c88a
Valente Huanca <rafayt092@gmail.com>
2026-05-12 11:24:59 -0600

    Add files via upload

---
+++ Arbol.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ARBOLES
+{
+    internal class ArbolBinarioBusqueda
+    {
+        private Nodo raiz;
+
+        public ArbolBinarioBusqueda()
+        {
+            raiz = null;
+        }
+
+        public bool EsVacio()
+        {
+            return raiz == null;
+        }
+
+
+
+    }
+}
---
+++ Nodo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ARBOLES
+{
+    public class Nodo
+    {
+        public int Dato;
+        public Nodo Izquierdo;
+        public Nodo Derecho;
+
+        public Nodo(int dato)
+        {
+            Dato = dato;
+            Izquierdo = null;
+            Derecho = null;
+        }
+    }
+}
